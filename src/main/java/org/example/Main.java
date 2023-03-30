1e09c486079b4c716a7bd480c2efd628b4ecaced
smedziko <smedziko@htl-steyr.ac.at>
2023-03-30 13:58:39 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+     getURLData();
+    }
+
+
+    public static void getURLData() throws IOException {
+
+        //region Scanner
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Geben Sie eine URL ein: ");
+        String urlInput = myObj.nextLine();  // Read user input
+        myObj.close();
+
+        //endregion
+
+
+
+        URL url = new URL("https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL?long=" + urlInput);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setDoInput(true);
+        httpURLConnection.setDoOutput(true);
+
+
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            JSONObject jsonObject = new JSONObject(response.toString());
+            System.out.println("https://1pt.co/" + jsonObject.get("short"));
+
+        }
+
+
+
+        /**
+        if (httpURLConnection.getResponseCode() == 200) {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            System.out.println(inputReader.readLine());
+        }
+         */
+
+
+    }
+}
